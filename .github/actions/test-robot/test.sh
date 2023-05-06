d180c01502e569113a9bf1b6e8177f54e3168edb
gergoszaszvaradi <szaszvaradi.gergo@yahoo.com>
2023-05-06 12:08:52 +0300

    exit code on fail, correct root dir for dev server

--- .github/actions/test-robot/test.sh
+++ .github/actions/test-robot/test.sh
@@ -1,6 +1,8 @@
-npx wrangler pages dev dist --local --persist &
+npx wrangler pages dev . --local --persist &
 pid=$!
 
 sleep 5
 robot --outputdir tests/robot/results tests/robot/greyboard.robot
+result=$?
 kill $pid
+exit $result
